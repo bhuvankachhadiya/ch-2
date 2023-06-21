30add2924cd92edb494c9796eec382a6ff5d9fc9
bhuvankachhadiya <137286464+bhuvankachhadiya@users.noreply.github.com>
2023-06-21 04:16:06 -0700

    Add files via upload

---
+++ SECOND.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+  clrscr();
+  int a,b,c,d,e,f,g;
+  a=10;
+  b=5;
+  c=a+b;
+  d=a-b;
+  e=a*b;
+  f=a/b;
+  g=a%b;
+  printf("a+b=%d",c);
+  printf("\na-b=%d",d);
+  printf("\na*b=%d",e);
+  printf("\na/b=%d",f);
+  printf("\na%%b=%d",g);
+  getch();
+  }
